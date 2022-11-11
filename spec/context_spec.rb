597116115002db979bb868b700728c8161bcf197
chucoyos <chucoyos@DESKTOP-D14FSCL.localdomain>
2022-11-10 22:38:40 -0600

    add nested describe methods

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,10 +1,15 @@
 RSpec.describe '#even? method' do
-  it 'should return true if number is even' do
-    expect(4.even?).to eq(true)
+
+  describe 'with even number' do
+    it 'should return true' do
+      expect(4.even?).to eq(true)
+    end
   end
 
-  it 'should return false if number is odd' do
-    expect(4.odd?).to eq(false)
+  describe 'with odd number' do
+    it 'should return false' do
+      expect(5.even?).to eq(false)
+    end
   end
 
 end
